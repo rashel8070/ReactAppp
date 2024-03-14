684f8224496581c1369bbee538c552994a6bde8f
TimiOkusi <148882249+TimiOkusi@users.noreply.github.com>
2024-03-14 13:41:28 +0000

    My new commit

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
+const API_URL = "https://didactic-journey-wrr7xj6v55rqc9r74-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
