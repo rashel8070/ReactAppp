86191418daff6e1322dcc9bf8734e86b71d6b992
TimiOkusi <148882249+TimiOkusi@users.noreply.github.com>
2024-04-20 13:50:25 +0000

    Added car Icon in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
 import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
 import { MdApi } from "react-icons/md";
+import carIcon from "/workspaces/Car-React-App/src/assets/carIcon.svg"
 
 const NavBar = () => {
   return (
     <Navbar className="bg-slate-100 h-16">
       <NavbarBrand>
-        <MdApi className="w-8 h-8 text-primary" />
+      <img
+        src={carIcon}
+        className="w-10 h-10 text-primary transform scale-x-[-1]"
+        style={{ transformOrigin: 'center' }}
+      />
         <p className="font-bold text-inherit">CAR API</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
